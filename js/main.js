0b293b644f7e65cbdc896f3197e0d889ebe4eff7
Carlo1891 <carlo.18@live.it>
2022-06-22 17:00:13 +0200

    Inizio Esercizio 2

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma.
 
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-
 // Consigli del giorno:
 // Scriviamo sempre in italiano i passaggi che vogliamo fare
 // Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
@@ -18,34 +11,30 @@
 // Prima parte con Palindromi
 // La parola anche se al contrario deve essere uguale.
 
-const userWord = prompt ('Scrivi una parola');
+// const userWord = prompt ('Scrivi una parola');
 
-function palindromes (userWord){
-    // La funzione deve riconoscere metà della composizione della parola, se uguale la parola sarà palindroma.
-    for (let index = 0; index < userWord.length / 2; index++)
-    // se la parola è uguale per tutta la sua metà, se non lo è
-    if (userWord [index] === userWord [userWord.length - 1 - index]){
-        console.log ('è una parola palindroma');
-    } else {
-        console.log ('Non è una parola palindroma');
-    }
-};
+// function palindromes (userWord){
+//     // La funzione deve riconoscere metà della composizione della parola, se uguale la parola sarà palindroma.
+//     for (let index = 0; index < userWord.length / 2; index++)
+//     // se la parola è uguale per tutta la sua metà, se non lo è
+//     if (userWord [index] === userWord [userWord.length - 1 - index]){
+//         console.log ('è una parola palindroma');
+//     } else {
+//         console.log ('Non è una parola palindroma');
+//     }
+// };
 
-alert (palindromes(userWord));
+// alert (palindromes(userWord));
 
+// Seconda Parte
 
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
-
-
-
-
-
-
-
-
-
-
-// Funzione per dopo, generatore numeri casuali.
-// function getRandom (min, max){
-//     return Math.floor(Math.random() * (max - min) + min);
-// }
+function getRandom (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
