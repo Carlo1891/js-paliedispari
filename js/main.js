2f7caf633870a8394bc15aeaf5734258988f3f8e
Carlo1891 <carlo.18@live.it>
2022-06-22 15:26:36 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// Consigli del giorno:
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// PS : nell'esercizio di oggi cercate di usare funzioni scritte da Voi ed evitate scorciatoie facile (tipo reverse giusto per dirne una). REVERSE = MHALEEEH
